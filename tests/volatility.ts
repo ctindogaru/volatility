b85c10dc78885a288a13717c9e6fba94e417ede0
Constantin Dogaru <ctin.dogaru95@gmail.com>
2022-12-06 16:49:51 +0200

    add test for the switchboard

--- tests/volatility.ts
+++ tests/volatility.ts
@@ -1,6 +1,11 @@
 import * as anchor from "@project-serum/anchor";
 import { Program } from "@project-serum/anchor";
 import { Volatility } from "../target/types/volatility";
+import { AggregatorAccount } from "@switchboard-xyz/switchboard-v2";
+import { SwitchboardTestContext } from "@switchboard-xyz/sbv2-utils";
+
+export const AGGREGATOR_PUBKEY: anchor.web3.PublicKey =
+  new anchor.web3.PublicKey("GvDMxPzN1sCj7L26YDK2HnMRXEQmQ2aemov8YBtPS7vR");
 
 describe("volatility", () => {
   // Configure the client to use the local cluster.
@@ -8,9 +13,35 @@ describe("volatility", () => {
 
   const program = anchor.workspace.Volatility as Program<Volatility>;
 
-  it("Is initialized!", async () => {
-    // Add your test here.
-    const tx = await program.methods.initialize().rpc();
+  let switchboard: SwitchboardTestContext;
+
+  before(async () => {
+    try {
+      switchboard = await SwitchboardTestContext.loadDevnetQueue(
+        program.provider as anchor.AnchorProvider
+      );
+      console.log("devnet detected");
+      return;
+    } catch (error: any) {
+      console.log(`Error: SBV2 Devnet - ${error.message}`);
+    }
+    // If fails, throw error
+    throw new Error(`Failed to load the SwitchboardTestContext from devnet`);
+  });
+
+  it("Reads an aggregator history buffer", async () => {
+    const aggregatorAccount = new AggregatorAccount({
+      program: switchboard.program,
+      publicKey: AGGREGATOR_PUBKEY,
+    });
+    const aggregator = await aggregatorAccount.loadData();
+
+    const tx = await program.methods
+      .calculateVolatility()
+      .accounts({
+        historyAccountInfo: aggregator.historyBuffer,
+      })
+      .rpc();
     console.log("Your transaction signature", tx);
   });
 });
