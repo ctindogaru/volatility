6680d15fb5a5eadcd4d1123e88e2188bcf68abb3
Constantin Dogaru <ctin.dogaru95@gmail.com>
2022-12-06 14:20:27 +0200

    change program id

--- programs/volatility/src/lib.rs
+++ programs/volatility/src/lib.rs
@@ -1,6 +1,6 @@
 use anchor_lang::prelude::*;
 
-declare_id!("Fg6PaFpoGXkYsidMpWTK6W2BeZ7FEfcYkg476zPFsLnS");
+declare_id!("FUWQwWXaBiu1h6b8tmTv2H1d4pvXD342ytC5vR3dwEX4");
 
 #[program]
 pub mod volatility {
